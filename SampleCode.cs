bc6ead4a7b36f144666946fcab1f45d0960a20a1
YAWAR MURTAZA KHUWAJA <yawar_yawar@hotmail.com>
2020-09-10 23:57:44 +0100

    Update SampleCode.cs

--- SampleCode.cs
+++ SampleCode.cs
@@ -1,45 +1,75 @@
-public class Employee : INotifyPropertyChanged
+public class Employee
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public DateTime DoB { get; set; }
+    public decimal AnnualSalary { get; set; }
+}
+
+
+public class EmployeeModel : INotifyPropertyChanged
 {
     private string firstName;
     public int Id { get; set; }
     public string FirstName
     {
-        get { return firstName; }
-        set
-        {
-            SetField(ref firstName, value, nameof(FirstName));
-        }
+        get => firstName;
+        set => SetField(ref firstName, value, nameof(FirstName));
     }
     public DateTime DoB { get; set; }
     public long AnnualSalary { get; set; }
 
-    public bool IsDirty { get; private set; }
+    public bool EmployeeFirstNameChanged { get; private set; }
 
     public event PropertyChangedEventHandler PropertyChanged;
 
 
     protected void SetField<T>(ref T field, T value, string propertyName)
     {
-        if (!EqualityComparer<T>.Default.Equals(field, value))
+        if (field != null)
         {
-            field = value;
-            IsDirty = true;
-            OnPropertyChanged(propertyName);
+            if (!EqualityComparer<T>.Default.Equals(field, value))
+            {
+                EmployeeFirstNameChanged = true;
+                //OnPropertyChanged(propertyName);
+            }
         }
+
+        field = value;
     }
 
-    [NotifyPropertyChangedInvocator]
-    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
+    
+    //protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    //{
+    //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    //}
 }
 
 
+
 // client code:
-Employee emp = new Employee();
-emp.FirstName = "Yawar"; // event fires...
+static void Main(string[] args)
+{
+    var mapper = new Mapper(ConfigureAutoMapper());
+    
+    Employee emp = new Employee();
+    emp.FirstName = "Yawar"; // event fires...
+    emp.Id = 33;
+    emp.AnnualSalary = 45000.00m;
+    emp.DoB = new DateTime(1900, 01, 10);
 
+    EmployeeModel model = new EmployeeModel();
+    mapper.Map<Employee, EmployeeModel>(emp, model);
 
-emp.FirstName = "Yawar2"; // event fires...
-emp.FirstName = "Yawar2"; // no event fires
+    // Note: model.EmployeeFirstNameChanged should be false.
+
+    emp.FirstName = "Yawar2";
+    mapper.Map<Employee, EmployeeModel>(emp, model);
+
+    // Note: model.EmployeeFirstNameChanged should be true.
+}
+
+static MapperConfiguration ConfigureAutoMapper()
+{
+    return new MapperConfiguration(cfg => cfg.CreateMap<Employee, EmployeeModel>());
+}
